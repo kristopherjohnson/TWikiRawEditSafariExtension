8281b9a8433c94c1e314f1e804e167e95933171d
Kristopher Johnson <kris@kristopherjohnson.net>
2013-04-18 12:50:49 -0400

    Add support for use with cloud.narwhalgroup.com

--- TransCoreWikiRawEdit.user.js
+++ TransCoreWikiRawEdit.user.js
@@ -7,45 +7,44 @@
 // @include http://cvs.tcore.com/twiki/bin/view/*
 // @include http://cvs.tcore.com/twiki/
 // @include https://metro.narwhalgroup.com/twiki/bin/view/*
-// @https://metro.narwhalgroup.com/twiki/
+// @include https://metro.narwhalgroup.com/twiki/
+// @include https://cloud.narwhalgroup.com/twiki/bin/view/*
+// @include https://cloud.narwhalgroup.com/twiki/
 // ==/UserScript==
 
-var changeEditLinks = function() {
-    console.log("TransCoreWikiRawEdit");
-    var editLinks = document.evaluate(
-        "//a[@title='Edit this topic text']",
-        document,
-        null,
-        XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-        null);
+(function () {
+    // Look for the Edit buttons on the page, and change them so that they
+    // open the raw-edit view rather than the WYSIWYG edit view.
+    var changeEditLinks = function() {
 
-    // There will actually be two matching elements, one
-    // at the top of the page and one at the bottom.
-    // We'll change the top one, so that the bottom
-    // one is still a WYSIWYG editor.
+        // Find <a> tags with the title "Edit this topic text".
+        var editLinks = document.evaluate(
+            "//a[@title='Edit this topic text']",
+            document,
+            null,
+            XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+            null);
 
-    if (editLinks.snapshotLength > 0) {
-        for (var i = 0; i < editLinks.snapshotLength; i++) {
-            var editLink = editLinks.snapshotItem(i);
+        if (editLinks.snapshotLength > 0) {
+            for (var i = 0; i < editLinks.snapshotLength; i++) {
+                var editLink = editLinks.snapshotItem(i);
 
-            // Set query parameter nowysiwyg=1
-            console.log(JSON.stringify(editLink.href));
-            if (editLink.href.indexOf(';nowysiwyg=0') >= 0)
-                editLink.href = editLink.href.replace(';nowysiwyg=0', ';nowysiwyg=1');
-            else
-                editLink.href += ';nowysiwyg=1';
-            console.log(JSON.stringify(editLink.href));
+                // Set query parameter nowysiwyg=1
+                if (editLink.href.indexOf(';nowysiwyg=0') >= 0)
+                    editLink.href = editLink.href.replace(';nowysiwyg=0', ';nowysiwyg=1');
+                else
+                    editLink.href += ';nowysiwyg=1';
 
-            editLink.title = 'Raw Edit this topic text';
-            editLink.accessKey = 'w';
-            editLink.innerHTML = 'Edit';
-        };
-    }
-}
+                editLink.title = 'Raw Edit this topic text';
+                editLink.accessKey = 'w';
+                editLink.innerHTML = 'Edit';
+            }
+        }
+    };
 
-// Changing the links doesn't work if we let this
-// run at the document-idle event, so we'll wait a
-// bit and then make the change.
+    // Changing the links doesn't work if we let this
+    // run at the document-idle event, so we'll wait a
+    // bit and then make the change.
 
-window.setTimeout(changeEditLinks, 100);
-
+    window.setTimeout(changeEditLinks, 100);
+})();
